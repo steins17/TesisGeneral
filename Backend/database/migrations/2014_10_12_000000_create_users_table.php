7dc2ee6b25b96a3faddc78f066b852efc2de2950
Santiago <santiagocordova2997@outlook.es>
2020-08-19 15:25:11 -0500

    cambios

--- Backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ Backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id')->autoIncrement();
             $table->string('name');
-            $table->string('email');
-            // $table->string('email')->unique();
+            // $table->string('email');
+            $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
