3777e1a5ffe8bc594e0fb8e80a99d81ac33b7736
Santiago <santiagocordova2997@outlook.es>
2020-11-28 10:31:36 -0500

    cambios finales

--- FrontEnd/src/apis/Persona.js
+++ FrontEnd/src/apis/Persona.js
@@ -2,18 +2,18 @@ import Api from "./Api";
 
 export default {
   recuperar(form) {
-    return Api().get("/perfil", form);
+    return Api().get("/perfil", form); 
   },
-  restablecer(form) {
-    return Api().post("/perfil", form );
+  restablecer(form) { 
+    return Api().post("/perfil", form ); 
   },
-  imagen(form) {
-    return Api().post("/imagen", form );
+  imagen(form) { 
+    return Api().post("/imagen", form ); 
   },
-  password(form) {
-    return Api().post("/password", form );
+  password(form) { 
+    return Api().post("/password", form ); 
   },
-  notas() {
-    return Api().get("/notastotales");
+  notas() { 
+    return Api().get("/notastotales"); 
   }
 }
