95554f2934cd3fb2e3ac7aa7cbae6b0ea3f1ff5e
Santiago <santiagocordova2997@outlook.es>
2020-11-28 10:51:11 -0500

    cambios

--- FrontEnd/src/views/Nivel1.vue
+++ FrontEnd/src/views/Nivel1.vue
@@ -4,43 +4,43 @@
       <ul class="nav nav-tabs" id="myTab" role="tablist">
         <li class="nav-item col-lg-4 text-center">
           <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true" style="border-radius: 50px">LETRAS DEL ABECEDARIO</a>
-        </li>
-        <li class="nav-item col-lg-4 text-center">
+        </li> 
+        <li class="nav-item col-lg-4 text-center"> 
           <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false" style="border-radius: 50px">COMPARACIÓN DE LETRAS</a>
-        </li>
+        </li> 
         <li class="nav-item col-lg-4 text-center">
           <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false" style="border-radius: 50px">Sílabas</a>
-        </li>
-      </ul> 
+        </li> 
+      </ul>    
       <div class="tab-content" id="myTabContent">
         <!-- letras -->
         <div class="tab-pane fade active show  mt-5" id="home" role="tabpanel" aria-labelledby="home-tab">
-          <vs-row>
+          <vs-row>  
             <vs-col vs-type="flex" vs-justify="center" vs-align="center" class="col-lg-4 col-md-6 p-0" v-for="(tr,index) in valores" :key="index">
               <div class="col-lg-12 mb-4">
-                <div class="card" style="border-radius: 50px">
-                  <img :src="'/archivos/imagenes/nivel2/letras/'+tr+'.png'" class="w-100" style="border-radius: 50px"/>
-                  <div class="card-head text-center">
-                    Mayúscula - Minúscula
-                  </div>
+                <div class="card" style="border-radius: 50px">  
+                  <img :src="'/archivos/imagenes/nivel2/letras/'+tr+'.png'" class="w-100" style="border-radius: 50px"/> 
+                  <div class="card-head text-center"> 
+                    Mayúscula - Minúscula 
+                  </div> 
                   <vs-row vs-justify="flex-end" style="position:absolute;bottom: 0px;display: block;">
                     <vs-button color="primary" type="gradient" style="float: right;margin-right: -12px;margin-bottom: -10px;--vs-color: 25, 91, 255;border-radius: 89px;width: 40px;height: 40px;" @click.prevent="sonido(tr)">
                       <vs-tooltip circle>
                         <i class="fas fa-volume-up fa-2x"></i>
                         <template #tooltip>
                           Escuchar
-                        </template>
-                      </vs-tooltip>
-                    </vs-button>
-                  </vs-row>
-                </div>
-              </div>
-            </vs-col>
-          </vs-row>
+                        </template>  
+                      </vs-tooltip> 
+                    </vs-button> 
+                  </vs-row> 
+                </div> 
+              </div> 
+            </vs-col> 
+          </vs-row> 
         </div>
-        <!-- comparacion letras -->
-        <div class="tab-pane fade mt-5" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-          <vs-row>
+        <!-- comparacion letras --> 
+        <div class="tab-pane fade mt-5" id="profile" role="tabpanel" aria-labelledby="profile-tab"> 
+          <vs-row> 
             <vs-col vs-type="flex" vs-justify="center" vs-align="center" w="6" v-for="(tr,index) in comparacion" :key="index">
               <div class="col-lg-12 mb-4"> 
                 <div class="card estilocard2" style="border-radius: 50px">
@@ -50,15 +50,15 @@
                   </div>
                   <vs-row vs-justify="flex-end" style="position:absolute;bottom: 0px;display: block;">
                     <vs-button color="primary" type="gradient" style="float: right;margin-right: -12px;margin-bottom: -10px;--vs-color: 25, 91, 255;border-radius: 89px;width: 40px;height: 40px;" @click.prevent="sonido(tr)">
-                      <vs-tooltip circle>
-                        <i class="fas fa-volume-up fa-2x"></i>
-                        <template #tooltip>
-                          Escuchar
-                        </template>
-                      </vs-tooltip>
-                    </vs-button>
-                  </vs-row>
-                </div>
+                      <vs-tooltip circle> 
+                        <i class="fas fa-volume-up fa-2x"></i> 
+                        <template #tooltip> 
+                          Escuchar 
+                        </template> 
+                      </vs-tooltip> 
+                    </vs-button> 
+                  </vs-row> 
+                </div> 
               </div>
             </vs-col>
           </vs-row>
@@ -75,20 +75,20 @@
                         <i class="fas fa-volume-up fa-2x"></i>
                         <template #tooltip>
                           Escuchar
-                        </template>
-                      </vs-tooltip>
-                    </vs-button>
-                  </vs-row>
-                </div>  
+                        </template> 
+                      </vs-tooltip> 
+                    </vs-button> 
+                  </vs-row> 
+                </div>   
             </vs-col>
-          </vs-row>
-        </div>
+          </vs-row> 
+        </div> 
       </div>
-    </div>
-  </div>
-</template>
-<script src="archivos/voz.js"></script>
-<script>
+    </div> 
+  </div> 
+</template> 
+<script src="archivos/voz.js"></script> 
+<script> 
 import store from "../store/store";
 const $ = require('jquery');
 export default {
