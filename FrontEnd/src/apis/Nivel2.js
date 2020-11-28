1b9f392a924dc386b9860c8156d288e5573d9554
Santiago <santiagocordova2997@outlook.es>
2020-11-28 10:30:07 -0500

    cambios

--- FrontEnd/src/apis/Nivel2.js
+++ FrontEnd/src/apis/Nivel2.js
@@ -1,7 +1,7 @@
-import Api from "./Api";
+import Api from "./Api"; 
 
-export default {
-  enviarletras(form) {
+export default { 
+  enviarletras(form) { 
     return Api().post("/nivel2/letras/respuesta", form);
   },
   enviarsilabas(form) {
@@ -10,44 +10,44 @@ export default {
   enviaroraciones(form) {
     return Api().post("/nivel2/oraciones/respuesta", form);
   },
-  llamarpreguntas() {
-    return Api().get("/nivel2/llamardatos");
+  llamarpreguntas() { 
+    return Api().get("/nivel2/llamardatos"); 
   },
-  llamarresultados() {
-    return Api().get("/nivel2/llamarresultados");
+  llamarresultados() { 
+    return Api().get("/nivel2/llamarresultados"); 
   },
   //administardor
-  listar() {
-    return Api().get("/nivel2/listar");
+  listar() { 
+    return Api().get("/nivel2/listar"); 
   },
-  guardar(form) {
-    return Api().post("/nivel2/guardar", form);
+  guardar(form) { 
+    return Api().post("/nivel2/guardar", form); 
   },
-  guardar_s(form) {
-    return Api().post("/nivel2/guardar_s", form);
+  guardar_s(form) { 
+    return Api().post("/nivel2/guardar_s", form); 
   },
-  guardar_oraciones(form) {
-    return Api().post("/nivel2/guardar_oraciones", form);
+  guardar_oraciones(form) { 
+    return Api().post("/nivel2/guardar_oraciones", form); 
   },
-  editar_l(form){
-    return Api().post("/nivel2/editar_l", form);
+  editar_l(form){ 
+    return Api().post("/nivel2/editar_l", form); 
   },
-  editar_s(form){
-    return Api().post("/nivel2/editar_s", form);
+  editar_s(form){ 
+    return Api().post("/nivel2/editar_s", form); 
   },
-  editar_o(form){
+  editar_o(form){ 
     return Api().post("/nivel2/editar_o", form);
   },
   eliminar_letras(id){
     return Api().delete("/nivel2/eliminar_letras/"+id);
   },
-  eliminar_silabas(id){
-    return Api().delete("/nivel2/eliminar_silabas/"+id);
+  eliminar_silabas(id){ 
+    return Api().delete("/nivel2/eliminar_silabas/"+id); 
   },
-  eliminar_oraciones(id){
-    return Api().delete("/nivel2/eliminar_oraciones/"+id);
+  eliminar_oraciones(id){ 
+    return Api().delete("/nivel2/eliminar_oraciones/"+id); 
   },
-  cambiar_estado(form){
-    return Api().post("/nivel2/cambiar_estado", form);
+  cambiar_estado(form){ 
+    return Api().post("/nivel2/cambiar_estado", form); 
   }
 }
